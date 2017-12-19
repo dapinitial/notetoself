6320573ffa1479098c4170b78166b79ccacad0ac
David Puerto <me@davidpuerto.com>
2017-12-19 02:00:52 -0800

    writing tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -4,7 +4,34 @@ import App from "./App";
 
 describe("App", () => {
   let app = mount(<App />);
-  it("renders the app", () => {
-    console.log(app.debug);
+  it("renders the app title", () => {
+    //console.log(app.debug());
+    expect(app.find("h2").text()).toEqual("Note to Self App");
+  });
+  it("renders the clear button", () => {
+    expect(
+      app
+        .find(".btn")
+        .at(1)
+        .text()
+    ).toEqual("Clear Notes");
+  });
+  describe("when rendering the form", () => {
+    it("creates a form component", () => {
+      expect(app.find("Form").exists()).toBe(true);
+    });
+
+    it("renders a FormControl component", () => {
+      expect(app.find("FormControl").exists()).toBe(true);
+    });
+
+    it("renders the submit button", () => {
+      expect(
+        app
+          .find(".btn")
+          .at(0)
+          .text()
+      ).toEqual("Submit");
+    });
   });
 });
